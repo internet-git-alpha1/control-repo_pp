dd05d8310c1001799592d62fafb8f13d4282e06d
Omar Moreno <omarmoreno20@gmail.com>
2018-08-21 20:06:48 +0000

    added bob user and group, with zsh package and ntp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,22 @@
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
-
+  include ntp
+  
+  package { 'zsh':
+    ensure => present,
+    }
+  user { 'bob':
+    endure  => present,
+    uid     => 1337,
+    gid     => 1337,
+    home    => '/home/bob',
+    shell   => '/bin/zsh'
+    managehome => true,
+    require => Package['zsh'],
+    
+    }
+   group { 'bob':
+    ensure  => present,
+    gid     => 1337,
+        }
 }
