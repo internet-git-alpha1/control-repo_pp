d92e0c190822a97bfbfe3d238efb27da8dc5bcd5
OmarMoreno <omarmoreno20@gmail.com>
2018-08-22 10:24:06 -0500

    cleaned whitespace

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,36 +1,36 @@
 class profile::base {
 
   include ntp
-  
+
   package { 'tree':
     ensure => installed,
     }
-    
+
   package { 'bash-completion':
     ensure => installed,
     }
-  
+
   package { 'zsh':
     ensure => present,
     }
-    
+
   user { 'bob':
-    ensure  => present,
-    uid     => 1337,
-    gid     => 1337,
-    home    => '/home/bob',
-    shell   => '/bin/zsh',
+    ensure     => present,
+    uid        => 1337,
+    gid        => 1337,
+    home       => '/home/bob',
+    shell      => '/bin/zsh',
     managehome => true,
-    require => Package['zsh'],
-    
+    require    => Package['zsh'],
+
     }
-   group { 'bob':
-    ensure  => present,
-    gid     => 1337,
+  group { 'bob':
+    ensure => present,
+    gid    => 1337,
         }
-    
+
     exec { 'install netdata':
-        command => 'bash <(curl -Ss https://my-netdata.io/kickstart.static64.sh)',
+        command  => 'bash <(curl -Ss https://my-netdata.io/kickstart.static64.sh)',
         privider => shell,
         }
 }
