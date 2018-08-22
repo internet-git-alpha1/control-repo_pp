e7dc985151d971c2d6fa421edacf2bfe48d2b6ba
OmarMoreno <omarmoreno20@gmail.com>
2018-08-22 13:52:05 -0500

    added comma for conext

--- site/profile/manifests/bash_prompt.pp
+++ site/profile/manifests/bash_prompt.pp
@@ -4,7 +4,7 @@
 class profile::base::base_prompt {
   file {'/etc/profile.d/puppet_bashprompt.sh':
     ensure => file,
-    content => 'export PS1="export PS1="\h@\u:\W\[$(tput sgr0)\]'
+    content => 'export PS1="export PS1="\h@\u:\W\[$(tput sgr0)\]',
     owner => 'root',
     group => 'root',
     root => '0644,'
