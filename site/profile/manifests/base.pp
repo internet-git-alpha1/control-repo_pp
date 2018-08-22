739d5a10558ce5e788b1840ce368c34e3a3033b8
OmarMoreno <omarmoreno20@gmail.com>
2018-08-22 12:26:59 -0500

    added netdata package

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,34 +3,49 @@ class profile::base {
   include ntp
 
   package { 'tree':
-    ensure => installed,
-    }
+      ensure    => installed,
+  }
 
   package { 'bash-completion':
-    ensure => installed,
-    }
+      ensure    => installed,
+  }
 
   package { 'zsh':
-    ensure => present,
+      ensure =>   present,
+  }
+
+  package { 'iptables':
+    ensure  => installed,
     }
 
   user { 'bob':
     ensure     => present,
     uid        => 1337,
     gid        => 1337,
-    home       => '/home/bob',
+    home       => '/home/shaq',
     shell      => '/bin/zsh',
     managehome => true,
-    require    => Package['zsh'],
+    require    => Package['zsh']
+  }
 
-    }
   group { 'bob':
-    ensure => present,
-    gid    => 1337,
-        }
+      ensure => present,
+      gid    => 1337,
+  }
 
-    exec { 'install netdata':
-        command  => 'bash <(curl -Ss https://my-netdata.io/kickstart.static64.sh)',
-        privider => shell,
-        }
+  exec { 'install netdata':
+    command => 'curl -Ss https://my-netdata.io/kickstart.sh | /bin/bash -s all --dont-wait --dont-start-it',
+    path    => ["/bin/", "/usr/bin/"],
+    unless  => "test -d /etc/netdata",
+  }
+
+  service { 'netdata':
+    ensure  => 'running',
+    require => Exec['install netdata'],
+  }
+
+  profile::iptables_rule { 'netdata':
+    rule    => '-t nat -I PREROUTING -p tcp --dport 8081 -j REDIRECT --to-ports 19999',
+    require => Service['netdata'],
+  }
 }
