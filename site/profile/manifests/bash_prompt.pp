3e804497f62519c1409c769fbcfda6eb2fa2125c
OmarMoreno <omarmoreno20@gmail.com>
2018-08-22 13:49:42 -0500

    syntax error correction

--- site/profile/manifests/bash_prompt.pp
+++ site/profile/manifests/bash_prompt.pp
@@ -1,7 +1,7 @@
 #Class: profile::base::base_prompt
 #
 #
-class profile::base:base_prompt {
+class profile::base::base_prompt {
   file {'/etc/profile.d/puppet_bashprompt.sh':
     ensure => file,
     content => 'export PS1="export PS1="\h@\u:\W\[$(tput sgr0)\]'
