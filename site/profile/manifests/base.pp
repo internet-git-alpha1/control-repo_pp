c32acbb8bfc0186d6658bb1cc47d559084ef7b74
OmarMoreno <omarmoreno20@gmail.com>
2018-08-23 09:33:56 -0500

    updated base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -48,9 +48,3 @@ class profile::base {
     rule    => '-t nat -I PREROUTING -p tcp --dport 8081 -j REDIRECT --to-ports 19999',
     require => Service['netdata'],
   }
-  
-if $facts['pe_server_version'] =~ /.+/ {
-  contain profile::base::base_prompt
-}
-
-}
